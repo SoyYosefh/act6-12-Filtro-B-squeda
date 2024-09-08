4ea7c0871432edfabb80f4c5fae3839beb378ff3
SoyYosefh <joratejedana@gmail.com>
2024-09-08 16:52:53 -0700

    Terminado

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-
+function filterList() {
+    // Obtener el valor de búsqueda
+    var searchInput = document.getElementById('searchInput').value.toLowerCase();
+    
+    // Obtener la lista de elementos
+    var items = document.querySelectorAll('#itemList li');
+    
+    // Filtrar los elementos según el texto ingresado
+    items.forEach(function(item) {
+        if (item.textContent.toLowerCase().includes(searchInput)) {
+            item.style.display = '';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+}
